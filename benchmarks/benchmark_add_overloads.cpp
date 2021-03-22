2d523ab1e10ab0ffd618f92f4c2876d0b3385974
Nikos Vasilas <vasilas.v.nikos@gmail.com>
2021-03-22 10:06:17 +0200

    Add benchmarks for operator+

--- benchmarks/benchmark_add_overloads.cpp
+++ benchmarks/benchmark_add_overloads.cpp
@@ -3,40 +3,67 @@
 #include <vector>
 
 using namespace poly;
-auto get_p1() { return Polynomial<int>{std::vector<int>(50000, 10)}; }
-auto get_p2() { return Polynomial<int>{std::vector<int>(1000, -1)}; }
 
-static void BM_add_lvalue(benchmark::State &state)
+class _Polynomial : public Polynomial<int>
+{
+  public:
+    _Polynomial(std::vector<int> coeff_) : m_coeff(coeff_) {}
+
+    friend _Polynomial operator+(_Polynomial &&lhs, const _Polynomial &rhs)
+    {
+        lhs += rhs;
+        return lhs;
+    }
+
+    friend _Polynomial operator+(const _Polynomial &lhs, _Polynomial &&rhs)
+    {
+        rhs += lhs;
+        return rhs;
+    }
+
+    auto &coefficients() const noexcept { return m_coeff; };
+
+  private:
+    std::vector<int> m_coeff;
+};
+
+const auto vec_large = std::vector<int>(50000, 10);
+const auto vec_small = std::vector<int>(1000, -1);
+
+static void BM_add_lvalues(benchmark::State &state)
 {
     for (auto _ : state) {
-        const auto p1 = get_p1();
-        const auto p2 = get_p2();
-        auto result = p1 + p2;
+        const auto large = Polynomial<int>{vec_large};
+        const auto small = Polynomial<int>{vec_small};
+
+        auto result = large + small;
         benchmark::DoNotOptimize(result.coefficients());
     }
 }
-BENCHMARK(BM_add_lvalue);
+BENCHMARK(BM_add_lvalues);
 
-static void BM_add_rvalue_first(benchmark::State &state)
+static void BM_add_big_rvalue_small_lvalue(benchmark::State &state)
 {
     for (auto _ : state) {
-        auto p1 = get_p1();
-        const auto p2 = get_p2();
-        auto result = std::move(p1) + p2;
+        auto large = _Polynomial{vec_large};
+        const auto small = _Polynomial{vec_small};
+
+        auto result = std::move(large) + small;
         benchmark::DoNotOptimize(result.coefficients());
     }
 }
-BENCHMARK(BM_add_rvalue_first);
+BENCHMARK(BM_add_big_rvalue_small_lvalue);
 
-static void BM_add_rvalue_second(benchmark::State &state)
+static void BM_add_big_lvalue_small_rvalue(benchmark::State &state)
 {
     for (auto _ : state) {
-        const auto p1 = get_p1();
-        auto p2 = get_p2();
-        auto result = p1 + std::move(p2);
+        const auto large = _Polynomial{vec_large};
+        auto small = _Polynomial{vec_small};
+
+        auto result = large + std::move(small);
         benchmark::DoNotOptimize(result.coefficients());
     }
 }
-BENCHMARK(BM_add_rvalue_second);
+BENCHMARK(BM_add_big_lvalue_small_rvalue);
 
 BENCHMARK_MAIN();
--- benchmarks/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo cpupower frequency-set --governor performance > /dev/null
-./$1
-sudo cpupower frequency-set --governor powersave > /dev/null
